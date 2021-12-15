3fde4dc8c82f1ba2c2444aaa93aedaa974a90fb9
davidmedinacortes <david.medina@soprasteria.com>
2021-12-15 16:56:50 +0100

    Integrated FireBase

--- app/src/main/java/com/dameco/postals/MainActivity.kt
+++ app/src/main/java/com/dameco/postals/MainActivity.kt
@@ -2,10 +2,17 @@ package com.dameco.postals
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Analytics Event
+        val analytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("message", "Integración FB completa")
+        analytics.logEvent("InitScreen", bundle)
     }
 }
